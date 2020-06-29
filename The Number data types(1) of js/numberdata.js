3cd1cc67af96ab0efbbf81fb4ef745ba8bab54f7
Faruk-2020 <59347740+Faruk-2020@users.noreply.github.com>
2020-06-29 09:49:53 +0200

    Add files via upload

---
+++ The Number data types(1) of js/numberdata.js
@@ -0,0 +1,11 @@
+ // Creating variables
+ var a = 25;
+ var b = 80.5;
+ var c = 4.25e+6;
+ var d = 4.25e-6;
+ 
+ // Printing variable values
+ document.write(a + "<br>");
+ document.write(b + "<br>");
+ document.write(c + "<br>");
+ document.write(d);
