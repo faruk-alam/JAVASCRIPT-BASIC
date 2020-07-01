254d68f8617e4eb306d7ee1d417a14f70cc50a10
Faruk-2020 <59347740+Faruk-2020@users.noreply.github.com>
2020-07-01 19:36:18 +0200

    Add files via upload

---
+++ the string data type of js/stringdata.js
@@ -0,0 +1,9 @@
+//single quote inside double quotes-
+//var a ="let's have a cup of tea";
+//document.write(a);
+//double quotes inside single quote-
+//var b ='He said that "I am ready"';
+//document.write(b);
+//escaping single quote with backslash
+var c ='we\'ll never give up';
+document.write(c);
