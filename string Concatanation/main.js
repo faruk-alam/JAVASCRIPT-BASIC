f1e243595895b32151bed583e93d3e56897c77cb
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-04-22 18:45:40 +0200

    Add files via upload

---
+++ string Concatanation/main.js
@@ -0,0 +1,7 @@
+//string concatenation text 
+var sen1 = "I am Faruk and";
+var sen2 =" 33 years old";
+document.write(sen1+sen2+"<br>");
+var sen__combined = sen1+sen2;
+document.write(sen__combined+"<br>");
+
