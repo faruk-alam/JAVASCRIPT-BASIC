7763f0c99522aed38adf3c6463e46243018ca118
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-04-24 21:13:01 +0200

    Add files via upload

---
+++ arithmatic operator increment decrement/main.js
@@ -0,0 +1,15 @@
+var x =10;
+document.write(++x);
+document.write(x);
+
+var x =10;
+document.write(x++);
+document.write(x);
+
+var x =10;
+document.write(--x);
+document.write(x);
+
+var x =10;
+document.write(x--);
+document.write(x);
