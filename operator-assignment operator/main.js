930d476037d5795745ca09c68cb729db38c92ef8
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-04-25 10:01:15 +0200

    Add files via upload

---
+++ operator-assignment operator/main.js
@@ -0,0 +1,26 @@
+var x;
+
+x = 10;
+document.write(x + "<br>");
+
+x = 10;
+x += 40;
+document.write(x + "<br>");
+
+x = 50;
+x -= 45;
+document.write(x + "<br>");
+
+x = 5;
+x *= 8;
+document.write(x + "<br>");
+
+x = 40,
+x /= 2;
+document.write(x + "<br>");
+
+x = 20;
+x %= 3;
+document.write(x + "<br>");
+
+
