690f793b7fc4e64c5bf186dabeb0f7e9f4fd255c
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-04-25 18:09:26 +0200

    Add files via upload

---
+++ operator-comparison operator/main.js
@@ -0,0 +1,25 @@
+var x = 25;
+var y = 35;
+var z = "25";
+document.write(x == z);
+document.write("<br>");
+
+document.write(x === z);
+document.write("<br>");
+
+document.write(x !=y);
+document.write("<br>");
+
+document.write(x !== z);
+document.write("<br>");
+
+document.write(x < y);
+document.write("<br>");
+
+document.write(x > y);
+document.write("<br>");
+
+document.write(x <= y);
+document.write("<br>");
+
+document.write(x >=y);
