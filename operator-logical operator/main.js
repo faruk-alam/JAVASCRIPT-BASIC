f56dcc7463be0d78280e99ecd6b8077fa41482d0
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-04-25 18:42:03 +0200

    Add files via upload

---
+++ operator-logical operator/main.js
@@ -0,0 +1,14 @@
+var x = 10;
+var y = 5;
+var z = 15;
+
+document.write(x > y && y < z);
+document.write("<br>");
+
+document.write(x>z && x>y);
+document.write("<br>");
+
+document.write(x>y || y>z);
+document.write("<br>")
+
+document.write(x<y || y>z);
