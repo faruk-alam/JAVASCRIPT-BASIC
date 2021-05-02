6487553f38f3b0c467db09de4653a8055b9b290f
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-02 10:30:12 +0200

    Add files via upload

---
+++ getting user output/main.js
@@ -0,0 +1,5 @@
+var name = prompt("What's your name ?");
+document.write("Hey, "+  name +", how are you");
+document.write("<br>");
+var time = prompt("what time have you arrived?");
+document.write("So,you have arrived at "+ time );
