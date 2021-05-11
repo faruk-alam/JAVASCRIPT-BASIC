6e2c193a90a0860389061640b240614482d1440b
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-11 18:45:28 +0200

    Add files via upload

---
+++ making basic calculator/main.js
@@ -0,0 +1,20 @@
+var firstnumber = prompt("ENTER FIRST NUMBER");
+var secondnumber = prompt("ENTER SECOND NUMBER");
+firstnumber = parseInt(firstnumber);
+secondnumber = parseInt(secondnumber);
+
+var result = firstnumber+secondnumber;
+document.write("The addition of two numbers is "+ result+"<br>");
+
+var result = firstnumber-secondnumber;
+document.write("The subtraction of two numbers is "+ result+"<br>");
+
+var result = firstnumber*secondnumber;
+document.write("The multiplacation of two numbers is "+ result+"<br>");
+
+var result = firstnumber/secondnumber;
+document.write("The division of two numbers is "+ result+"<br>");
+
+var result = firstnumber%secondnumber;
+document.write("The mudolus of two numbers is "+ result+"<br>");
+
