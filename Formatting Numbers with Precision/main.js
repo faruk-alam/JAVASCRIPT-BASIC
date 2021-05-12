c69c232661ea63c2c21dab1280bdfa141d99b8bb
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-12 15:08:48 +0200

    Add files via upload

---
+++ Formatting Numbers with Precision/main.js
@@ -0,0 +1,10 @@
+var a = 1.246;
+document.write(a.toPrecision() + "<br>");
+document.write(a.toPrecision(1) + "<br>");
+document.write(a.toPrecision(2) + "<br>");
+document.write(a.toPrecision(3) + "<br>");
+var b = 54.76;
+document.write(b.toPrecision(2)+"<br>");
+var c = 12455654;
+document.write(c.toPrecision(2))
+
