4ca853652a8d3262206e2a0c2ef6d64030841830
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-12 15:49:16 +0200

    Add files via upload

---
+++ Formatting Numbers to Fixed Decimals/main.js
@@ -0,0 +1,9 @@
+var a = 44.63456;
+document.write(a.toFixed()+"<br>");
+document.write(a.toFixed(1)+"<br>");
+document.write(a.toFixed(2)+"<br>");
+document.write(a.toFixed(3)+"<br>");
+var b = 1.23e+6;
+document.write(b.toFixed(2)+"<br>");
+var c =1.24e-6;
+document.write(c.toFixed(2)+"<br>");
