93dac5d96c58699ff9118e34d5c0f4160435ba9d
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-13 18:43:44 +0200

    Add files via upload

---
+++ string method-Extracting a Substring from a String/main.js
@@ -0,0 +1,6 @@
+var str = "The quick brown fox jumps over the lazy dog.";
+document.write(str.slice(20,30));
+document.write("<br>");
+var str = "The quick brown fox jumps over the lazy dog.";
+document.write(str.slice(-34,-25));
+
