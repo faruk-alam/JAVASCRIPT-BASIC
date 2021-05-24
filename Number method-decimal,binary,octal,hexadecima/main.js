47fe908534e2ed180bbdacc0914e648bb3f2e687
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-24 10:31:20 +0200

    Add files via upload

---
+++ Number method-decimal,binary,octal,hexadecima/main.js
@@ -0,0 +1,6 @@
+var input = Number(prompt("put a number"))
+
+ document.write("decimal       :"+ input+"<br>");
+ document.write("binary        :"+ input.toString(2)+"<br>");
+ document.write("octal         :"+ input.toString(8)+"<br>");
+ document.write("hexadecimal   :"+ input.toString(16)+"<br>");
