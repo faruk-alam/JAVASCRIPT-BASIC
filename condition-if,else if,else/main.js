11d59b42dd7ddc0105eb7b1422a8ffb462a1d4a7
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-05-30 19:08:12 +0200

    Add files via upload

---
+++ condition-if,else if,else/main.js
@@ -0,0 +1,11 @@
+var hour = prompt("what's time right now?");
+if(hour>=6 && hour<12){
+    document.write("Good Morning");
+}
+else if(hour>=12 && hour< 18){
+    document.write("Good Afternoon");
+}
+else{
+    document.write("Good Evening");
+}
+    
