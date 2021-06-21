17df1768197d4390ddc7e74db52b3a3724e5ec2c
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-06-21 14:31:09 +0200

    Add files via upload

---
+++ loop-break and continue/main.js
@@ -0,0 +1,17 @@
+/*for (var a = 1;a < 100;a++){
+   if(a == 9){
+     break;
+   }
+   document.write(a+" ");
+}
+document.write("end");*/
+
+for (var a = 1;a < 100;a++){
+   if(a == 9){
+    continue;
+   }
+   document.write(a+" ");
+}
+document.write("end");
+
+
