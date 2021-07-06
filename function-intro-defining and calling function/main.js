248406a1cc0d481060f3b433468235cf94ca7396
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-06 11:27:03 +0200

    Add files via upload

---
+++ function-intro-defining and calling function/main.js
@@ -0,0 +1,10 @@
+// Defining function
+function greeting(){
+   document.write("hey,what's up every body" + "<br>");
+}
+// Calling function
+greeting();
+greeting();
+/*Once a function is defined it can be called (invoked) from anywhere in the document,
+ by typing its name followed by a set of parentheses, like greeting() in the example 
+ above*/
