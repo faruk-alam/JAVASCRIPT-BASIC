11ef452d1584964e1315292e1d7cee1366d663d9
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-07 17:55:40 +0200

    Add files via upload

---
+++ function-intro-Adding Parameters-1/main.js
@@ -0,0 +1,14 @@
+// Defining function
+function displaysum(num1,num2){
+   var total=num1+num2;
+   document.write(total+"<br>");
+
+}
+// Calling function
+displaysum(40,10);
+displaysum(8,7);
+displaysum(99,1);
+
+/*The displaySum() function in the following example takes two numbers as arguments, 
+simply add them together and then display the result in the browser.*/
+
