134b514206c0e8c61ca905854282cacded7b7823
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-07 18:50:54 +0200

    Add files via upload

---
+++ function-intro-Adding Parameters-2/main.js
@@ -0,0 +1,12 @@
+// Defining function
+function fullname(firstname,lastname){
+   document.write(firstname + " " + lastname +"<br>");
+
+}
+// Calling function
+fullname("mikeal","Jackson");
+fullname("Mikeal")
+
+/*The displaySum() function in the following example takes two numbers as arguments, 
+simply add them together and then display the result in the browser.*/
+
