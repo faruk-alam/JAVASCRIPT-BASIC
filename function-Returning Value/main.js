0420dc7169a6ecbe90b3f1fc9fcda5d8e9f9ef2a
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2021-07-08 15:49:41 +0200

    Add files via upload

---
+++ function-Returning Value/main.js
@@ -0,0 +1,11 @@
+// Defining function
+function getsum(num1,num2){
+   var total=num1+num2;
+   return total;
+}
+// Calling function
+document.write(getsum(5,6));
+
+/*The displaySum() function in the following example takes two numbers as arguments, 
+simply add them together and then display the result in the browser.*/
+
